97c968a15ae3c16e7b8f32a0a67dc12abf227b36
Jonathan Chen <jonathanchenzheng@hotmail.com>
2020-07-23 20:52:31 -0400

    Install Material UI

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React from 'react'
 import './App.css'
-import { Container, Row, Col, Card, CardBody, CardImg } from "shards-react"
-import image from "./test.jpg"
 
-const list = ["1", "2", "3", "4", "5", "6", "7"]
 function App() {
   return (
     <div class="row">
-      {list.map(card =>
-        <div class="card small">
-            <img src={image} class="section media"/>
-            <h2>Card {card}</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum dictum, risus ut euismod euismod, erat mi aliquam erat, eu venenatis nulla metus mattis ante.</p>
-        </div>
-      )}
-
+      
       
       
     </div>
